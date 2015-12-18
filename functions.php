f0fcb7f54b5877eeef5f5dae91fc57ea2436246a
Yuzuru Sano <sano@suitosha.co,jp>
2015-12-18 12:37:04 +0900

    いきなりエラーが出るので修正

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@ get_template_part( 'functions/custom_post' );//カスタム投稿まとめ
 get_template_part( 'functions/admin' );//管理画面カスタマイズ
 get_template_part( 'functions/post_utility');//記事作成時やテンプレート作成時に便利な機能
 get_template_part( 'functions/walker');//walkerのextendがあればここで。
-get_template_part( 'functions/custom_post' );//カスタム投稿まとめ
 get_template_part( 'functions/comments' );//コメント設定
 get_template_part( 'functions/plugin_extend');//プラグイン機能拡張
 ?>
